484ceb8dfec1c0781febcb242b0bc4ec36704d43
JatinBumbra <56374584+JatinBumbra@users.noreply.github.com>
2020-07-25 00:28:08 +0530

    Project ready for deployement... Bug: Fix the like, unlike and delete post malfunctioning

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Alert from "./components/layout/Alert";
-import Dashboard from "./components/dashboard/Dashboard";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-form/CreateProfile";
-import EditProfile from "./components/profile-form/EditProfile";
-import AddExperience from "./components/profile-form/AddExperience";
-import AddEducation from "./components/profile-form/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
+import Routes from "./components/routing/Routes";
+
 // Redux
 import store from "./store";
 import { Provider } from "react-redux";
@@ -34,38 +25,10 @@ function App() {
 			<Router>
 				<React.Fragment>
 					<Navbar />
-					<Route exact path="/" component={Landing} />
-					<div className="container">
-						<Alert />
-						<Switch>
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/register" component={Register} />
-							<Route exact path="/profiles" component={Profiles} />
-							<Route exact path="/profile/:id" component={Profile} />
-
-							<PrivateRoute exact path="/dashboard" component={Dashboard} />
-							<PrivateRoute
-								exact
-								path="/create-profile"
-								component={CreateProfile}
-							/>
-							<PrivateRoute
-								exact
-								path="/edit-profile"
-								component={EditProfile}
-							/>
-							<PrivateRoute
-								exact
-								path="/add-experience"
-								component={AddExperience}
-							/>
-							<PrivateRoute
-								exact
-								path="/add-education"
-								component={AddEducation}
-							/>
-						</Switch>
-					</div>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route component={Routes} />
+					</Switch>
 				</React.Fragment>
 			</Router>
 		</Provider>
